6e389a3ac265912347c98bbd0b65c5d0ad407926
Rob Ervin Jauquet <rob.jauquet@zapier.com>
2019-01-13 12:00:02 -0600

    adding HasAPIAccessOrIsAuthenticated perm

--- rest_framework_api_key/permissions.py
+++ rest_framework_api_key/permissions.py
@@ -8,3 +8,13 @@ class HasAPIAccess(permissions.BasePermission):
     def has_permission(self, request, view):
         api_key = request.META.get('HTTP_API_KEY', '')
         return APIKey.objects.filter(key=api_key).exists()
+
+class HasAPIAccessOrIsAuthenticated(permissions.BasePermission):
+    """Authorize if a valid API key is provided or request is authenticated."""
+
+    def has_permission(self, request, view):
+        perms = [
+            HasAPIAccess(),
+            permissions.IsAuthenticated(),
+        ]
+        return any(perm.has_permission(request, view) for perm in perms)
